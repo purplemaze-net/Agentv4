ff719e5f4389bb938fea0889cb8c2b0250f9fa18
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2024-11-19 22:36:02 +0100

    FirewallManager: Definitions

--- src/FirewallManager.cs
+++ src/FirewallManager.cs
@@ -11,9 +11,75 @@ public class FirewallManager {
         Servers = servers;
     }
 
+    /// <summary>
+    ///  Called at startup. Will call the good linux/windows method to create the base rules / check if it can operate.
+    /// </summary>
+    public static bool InitFirewall(){
+#if WINDOWS
+        return InitFirewallWindows();
+#elif LINUX
+        return InitFirewallLinux();
+#endif
+        return false;
+    }
+
+    /// <summary>
+    ///  Init the firewall for Windows.
+    /// </summary>
+    private static bool InitFirewallWindows() {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Init the firewall for Linux.
+    /// </summary>
+    private static bool InitFirewallLinux() {
+        throw new NotImplementedException();
+    }
+
     // Make the initial IP requests & whitelist them
     public void Init(){
 
+    }
+
+    /// <summary>
+    ///  Authorize on windows Firewall for address on port (TCP+UDP)
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    private bool AddOnWindows(string address, ushort port){
+        throw new NotImplementedException();   
+    }
+
+    /// <summary>
+    ///  Authorize on iptables for address on port (TCP+UDP)
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    private bool AddOnLinux(string address, ushort port){
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    ///  De-authorize on windows Firewall for address on port (TCP+UDP)
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    private bool RemoveOnWindows(string address, ushort port){
+        throw new NotImplementedException();   
+    }
+
+    /// <summary>
+    ///  De-authorize on iptables for address on port (TCP+UDP)
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    private bool RemoveOnLinux(string address, ushort port){
+        throw new NotImplementedException();
     }
 
     public bool AddWhitelist(string slug, IPAddress ip){
