e604acd5ca7c2c610b72e54b441e9efac4c2f000
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2024-11-25 12:35:35 +0100

    Program: Load certificate from embedded ressources

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-using PPMV4.Agent.Logging;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 class Agent
 {
@@ -13,5 +15,34 @@ class Agent
 #elif LINUX
         new Log("Platform: Linux");
 #endif
+
+        byte[] publicCert = GetRessource("Purplemaze-Agent.keys.public.key");
+        X509Certificate2 cert = new(publicCert);
+
+        // Start web server
     }
+
+    /// <summary>
+    ///  Get content from an embedded resource
+    /// </summary>
+    /// <param name="name">Name of the resource</param>
+    static byte[] GetRessource(string name) {
+        var assembly = Assembly.GetExecutingAssembly();
+        string[] ressources = assembly.GetManifestResourceNames();
+        
+        foreach (string ressource in ressources) {
+            if (ressource == name) {
+                using (Stream? stream = assembly.GetManifestResourceStream(ressource))
+                if(stream != null){
+                    using (StreamReader reader = new(stream)) {
+                        return Encoding.UTF8.GetBytes(reader.ReadToEnd());
+                    }
+                }
+            }
+        }
+
+        // If not found or null somewhere
+        throw new Exception("Resource not found");
+    }
+
 }
